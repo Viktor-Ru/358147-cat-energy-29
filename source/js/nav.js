b6604133c384bec22581f2c34d7a74441f6b3943
Viktor <viktor-876@yandex.ru>
2023-12-30 01:51:07 +0300

    Добавляет JS для меню

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,15 +1,11 @@
-let navMain = document.querySelector('.navigation-list');
-let navToggle = document.querySelector('.navigation__toggle');
+function toggleMenu (list, toggle) {
 
-navMain.classList.add('navigation-list--js');
-navToggle.classList.add('navigation__toggle--js');
-
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('navigation-list--open')) {
-    navMain.classList.remove('navigation-list--open');
-    navToggle.classList.remove('navigation__toggle--open');
-  } else {
-    navMain.classList.add('navigation-list--open');
-    navToggle.classList.add('navigation__toggle--open');
+  function onToggleClick () {
+    list.classList.toggle('navigation-list--open');
+    toggle.classList.toggle('navigation__toggle--open');
   }
-});
+
+  toggle.addEventListener('click', onToggleClick);
+}
+
+export { toggleMenu };
